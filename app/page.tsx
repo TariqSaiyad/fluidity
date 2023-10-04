b25c89ca60c110a2ebed5de8971e36ba03236acc
Tariq Saiyad <tariqsaiyad98@gmail.com>
2023-10-05 08:06:32 +1300

    basic implementation done

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,112 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { Button, Container, Group, NumberInput, Paper } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useClipboard } from "@mantine/hooks";
+import { useState } from "react";
+
+interface FluidityForm {
+  minScreen: number;
+  maxScreen: number;
+  minFont: number;
+  maxFont: number;
+}
+
+const REM = 16;
+
+const toRem = (value: number) => value / REM;
+const rounded = (value: number, place = 4) => parseFloat(value.toFixed(place));
+
+const getClampCSS = (clampValue: string) => `--fluid-scale: ${clampValue};`;
+
+function getClampValue(data: FluidityForm | null) {
+  if (!data) return "";
+
+  const { maxFont, maxScreen, minFont, minScreen } = data;
+
+  const slope = (maxFont - minFont) / (maxScreen - minScreen);
+  const yIntersection = -1 * minScreen * slope + minFont;
+
+  return `clamp(${toRem(minFont)}rem, ${rounded(
+    toRem(yIntersection)
+  )}rem + ${rounded(slope * 100)}vw, ${toRem(maxFont)}rem)`;
+}
 
 export default function Home() {
+  const clipboard = useClipboard({ timeout: 1000 });
+
+  const [formData, setFormData] = useState<FluidityForm | null>(null);
+  const form = useForm<FluidityForm>({
+    initialValues: {
+      minScreen: 375,
+      minFont: 16,
+      maxScreen: 1440,
+      maxFont: 18,
+    },
+    validate: {
+      minScreen: (value) => (value < 0 ? "Must be greater than 0" : null),
+      minFont: (value) => (value < 0 ? "Must be greater than 0" : null),
+      maxScreen: (value) => (value < 0 ? "Must be greater than 0" : null),
+      maxFont: (value) => (value < 0 ? "Must be greater than 0" : null),
+    },
+  });
+
+  const handleSubmit = (data: FluidityForm) => setFormData(data);
+
+  const clampValue = getClampValue(formData);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <main>
+      <Container>
+        <h1>Fluidity</h1>
+
+        <Paper shadow="lg" radius="lg" p="xl">
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <NumberInput
+              label="min font"
+              description="min font"
+              {...form.getInputProps("minFont")}
             />
-          </a>
-        </div>
-      </div>
+            <NumberInput
+              label="min Screen"
+              description="min Screen"
+              {...form.getInputProps("minScreen")}
+            />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <NumberInput
+              label="max font"
+              description="max font"
+              {...form.getInputProps("maxFont")}
+            />
+            <NumberInput
+              label="max Screen"
+              description="max Screen"
+              {...form.getInputProps("maxScreen")}
+            />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <Group justify="flex-end" mt="md">
+              <Button type="submit">Submit</Button>
+            </Group>
+          </form>
+        </Paper>
+        <Paper shadow="lg" radius="lg" p="xl" mt="xl">
+          {JSON.stringify(formData, null, 2)}
+        </Paper>
+        <Paper shadow="lg" radius="lg" p="xl" mt="xl">
+          <Button
+            color={clipboard.copied ? "teal" : "blue"}
+            onClick={() => clipboard.copy(getClampCSS(clampValue))}
+          >
+            {clipboard.copied ? "Copied" : "Copy"}
+          </Button>
+          <pre>
+            :root {"{"}
+            <br />
+            {"  "}--fluid-scale: {clampValue};
+            <br />
+            {"}"}
+          </pre>
+        </Paper>
+      </Container>
     </main>
-  )
+  );
 }
